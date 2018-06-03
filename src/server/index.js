3b19bc6f018fc4c4d128a7a3c6b7e247788d7c15
Jack Ford <jford8820@gmail.com>
2018-06-03 19:32:05 +0200

    Fix typo

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { StaticRouter } from 'react';
+import { StaticRouter } from 'react-router';
 
 import { SheetsRegistry } from 'react-jss/lib/jss';
 import JssProvider from 'react-jss/lib/JssProvider';
