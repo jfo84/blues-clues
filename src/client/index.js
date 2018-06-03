b702e65b45cda91078047ced2facb45e62d85168
Jack Ford <jford8820@gmail.com>
2018-06-03 22:10:37 +0200

    It works

--- src/client/index.js
+++ src/client/index.js
@@ -22,15 +22,9 @@ const theme = createMuiTheme({
   }
 });
 
-// // Grab the state from a global variable injected into the server-generated HTML
-// const preloadedState = window.__PRELOADED_STATE__;
-// ​
-// // Allow the passed state to be garbage-collected
-// delete window.__PRELOADED_STATE__;
-
 export const store = createStore(
   reducer,
-  // preloadedState,
+  window.__PRELOADED_STATE__,
   applyMiddleware(thunk)
 );
 
